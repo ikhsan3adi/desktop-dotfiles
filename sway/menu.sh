aeceb7d2ddb29e9ecf1b9e45c240a64a59e6cc11
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-07-03 14:26:08 +0700

    feat: improve waybar and sway config
    - Update waybar config and style
      - Increase font size
    - Increase font size on wmenu

--- sway/menu.sh
+++ sway/menu.sh
@@ -3,7 +3,7 @@ source ~/.config/sway/colors.sh
 wmenu-run \
   -i \
   -p Search: \
-  -f "JetBrainsMono Nerd Font Propo 14" \
+  -f "JetBrainsMono Nerd Font Propo 15" \
   -N $base \
   -n $subtext1 \
   -M $transparent \
