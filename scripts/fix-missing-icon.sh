1e21ba39232332708f338cbccdaa5e492b1239d5
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2026-01-26 10:25:12 +0700

    fix(scripts): add script to fix missing blender-lts icon
    Creates symlinks for blender-lts.svg to point to blender.svg in Papirus
    icon theme, resolving missing icon issues for Blender LTS.

---
+++ scripts/fix-missing-icon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ICONS_DIR=~/.icons
+ICONS_DIR=/usr/share/icons
+
+ln -s $ICONS_DIR/Papirus/16x16/apps/blender.svg $ICONS_DIR/Papirus/16x16/apps/blender-lts.svg
+ln -s $ICONS_DIR/Papirus/22x22/apps/blender.svg $ICONS_DIR/Papirus/22x22/apps/blender-lts.svg
+ln -s $ICONS_DIR/Papirus/24x24/apps/blender.svg $ICONS_DIR/Papirus/24x24/apps/blender-lts.svg
+ln -s $ICONS_DIR/Papirus/32x32/apps/blender.svg $ICONS_DIR/Papirus/32x32/apps/blender-lts.svg
+ln -s $ICONS_DIR/Papirus/48x48/apps/blender.svg $ICONS_DIR/Papirus/48x48/apps/blender-lts.svg
+ln -s $ICONS_DIR/Papirus/64x64/apps/blender.svg $ICONS_DIR/Papirus/64x64/apps/blender-lts.svg
+ln -s $ICONS_DIR/Papirus/96x96/apps/blender.svg $ICONS_DIR/Papirus/96x96/apps/blender-lts.svg
+ln -s $ICONS_DIR/Papirus/128x128/apps/blender.svg $ICONS_DIR/Papirus/128x128/apps/blender-lts.svg
