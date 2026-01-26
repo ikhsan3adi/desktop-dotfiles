7d4e97dfab65b59d4bf56f1724a8bf638d89c7f2
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2026-01-26 10:22:32 +0700

    feat(vscodium): add config install script
    Creates symbolic links for settings.json and keybindings.json into the VSCodium and Antigravity user configuration directories.

---
+++ VSCodium/install.sh
@@ -0,0 +1,7 @@
+# VSCodium
+ln -s $(pwd)/settings.json ~/.config/VSCodium/User/settings.json
+ln -s $(pwd)/keybindings.json ~/.config/VSCodium/User/keybindings.json
+
+# Antigravity
+ln -s $(pwd)/settings.json ~/.config/Antigravity/User/settings.json
+ln -s $(pwd)/keybindings.json ~/.config/Antigravity/User/keybindings.json
