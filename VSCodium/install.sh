ee36bcc2cbac3819bef8be2eb9fb490f076b3c56
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2026-05-26 22:33:00 +0700

    fix(code-oss): update antigravity path

--- VSCodium/install.sh
+++ VSCodium/install.sh
@@ -4,16 +4,26 @@ ln -s $(pwd)/keybindings.json ~/.config/VSCodium/User/keybindings.json
 ln -s $(pwd)/snippets ~/.config/VSCodium/User
 
 # Antigravity
-ln -s $(pwd)/settings.json ~/.config/Antigravity/User/settings.json
-ln -s $(pwd)/keybindings.json ~/.config/Antigravity/User/keybindings.json
-ln -s $(pwd)/snippets ~/.config/Antigravity/User
+ln -s $(pwd)/settings.json '/home/dydx/.config/Antigravity IDE/User/settings.json'
+ln -s $(pwd)/keybindings.json '/home/dydx/.config/Antigravity IDE/User/keybindings.json'
+ln -s $(pwd)/snippets '/home/dydx/.config/Antigravity IDE/User'
 
 # Windsurf
 ln -s $(pwd)/settings.json ~/.config/Windsurf/User/settings.json
 ln -s $(pwd)/keybindings.json ~/.config/Windsurf/User/keybindings.json
 ln -s $(pwd)/snippets ~/.config/Windsurf/User
 
+# Windsurf Next
+ln -s $(pwd)/settings.json '/home/dydx/.config/Windsurf - Next/User/settings.json'
+ln -s $(pwd)/keybindings.json '/home/dydx/.config/Windsurf - Next/User/keybindings.json'
+ln -s $(pwd)/snippets '/home/dydx/.config/Windsurf - Next/User'
+
 # Cursor
 ln -s $(pwd)/settings.json ~/.config/Cursor/User/settings.json
 ln -s $(pwd)/keybindings.json ~/.config/Cursor/User/keybindings.json
 ln -s $(pwd)/snippets ~/.config/Cursor/User
+
+# VSCode
+ln -s $(pwd)/settings.json ~/.config/Code/User/settings.json
+ln -s $(pwd)/keybindings.json ~/.config/Code/User/keybindings.json
+ln -s $(pwd)/snippets ~/.config/Code/User
--- VSCodium/symlink-extensions.sh
+++ VSCodium/symlink-extensions.sh
@@ -1,5 +1,6 @@
 SOURCE=".vscode-oss" # VSCodium
 
 ln -s ~/$SOURCE/extensions ~/.windsurf/extensions
-ln -s ~/$SOURCE/extensions ~/.antigravity/extensions
+ln -s ~/$SOURCE/extensions ~/.antigravity-ide/extensions
 ln -s ~/$SOURCE/extensions ~/.cursor/extensions
+ln -s ~/$SOURCE/extensions ~/.vscode/extensions
